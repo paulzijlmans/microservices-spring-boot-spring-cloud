910a48c56ad9af481617444fb647ef42bb0aa14a
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-20 15:55:34 +0200

    Implement API

--- microservices/product-composite-service/src/main/java/nl/paulzijlmans/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/nl/paulzijlmans/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -2,8 +2,10 @@ package nl.paulzijlmans.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("nl.paulzijlmans")
 public class ProductCompositeServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/recommendation-service/src/main/java/nl/paulzijlmans/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/nl/paulzijlmans/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package nl.paulzijlmans.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("nl.paulzijlmans")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
